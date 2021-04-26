2866ba8588b2ad175fc52ccdea72e8055d4b5f46
alex <jalexcole@gmail.com>
2021-04-26 16:47:48 -0500

    Collects stock information from the dow jones industrial average and doge-coin

--- src/main/java/io/cole/collector/scheduledtasks/ScheduledTasks.java
+++ src/main/java/io/cole/collector/scheduledtasks/ScheduledTasks.java
@@ -1,7 +1,13 @@
 package io.cole.collector.scheduledtasks;
+import java.io.*;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cole.collector.domain.FetchStock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,7 +21,7 @@ public class ScheduledTasks {
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
-    @Scheduled(fixedRate = 60 * 1000)
+    @Scheduled(fixedRate = 2 * 60 * 1000)
     public void reportCurrentTime() {
         // Place Scheduled tasks Here
         log.info("The time is now {}", dateFormat.format(new Date()));
@@ -31,5 +37,38 @@ public class ScheduledTasks {
         // Process List
         FetchStock fetchedStock = new FetchStock("DOGE-USD");
         log.info(fetchedStock.toJSON());
+
+        List<String> stocks = fetchStocks();
+        for (String stockSymbol: stocks) {
+            fetchedStock = new FetchStock(stockSymbol);
+            log.info(fetchedStock.toJSON());
+        }
+    }
+
+    public List<String> fetchStocks() {
+        String path = "src/main/resources/static/";
+        ObjectMapper objectMapper = new ObjectMapper();
+        InputStream inputStream;
+
+        List<String> stocks = new ArrayList<String>();
+        try {
+            inputStream = new FileInputStream(path + "dji.json");
+
+            try {
+                stocks = objectMapper.readValue(inputStream, new TypeReference<List<String>>(){});
+
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+
+        return stocks;
     }
 }
